17fe76f3ed05ffcf0d4809063221ccd03d8421af
MADHAV BAHL <madhavbahl10@gmail.com>
2017-06-19 18:25:20 +0530

    CODE 7 and 8 DONE

--- Fancy light 5 LED/led/led.ino
+++ Fancy light 5 LED/led/led.ino
@@ -1,9 +1,38 @@
+int ledPin[5] = { 13 , 12 ,11 , 10 , 9 };
+int count = 0;
 void setup() {
-  // put your setup code here, to run once:
-
+  int i;
+  for(i=0;i<5;i++)
+  {
+    pinMode(ledPin[i],OUTPUT);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  int i;
+  for(i=0;i<5;i++)
+  {
+    digitalWrite(ledPin[i],HIGH);
+    delay(250);
+    digitalWrite(ledPin[i],LOW);
+  }
+  for(i=3;i>0;i--)
+  {
+    digitalWrite(ledPin[i],HIGH);
+    delay(250);
+    digitalWrite(ledPin[i],LOW);
+  }
+  count++;
+  if(count%2 == 0)
+  {
+    for(i=0;i<6;i++)
+    {
+      digitalWrite(ledPin[1],HIGH);
+      delay(150);
+      digitalWrite(ledPin[1],LOW);
+      digitalWrite(ledPin[3],HIGH);
+      delay(150);
+      digitalWrite(ledPin[3],LOW);
+    }
+  }
 }
--- LDR/ldr/ldr.ino
+++ LDR/ldr/ldr.ino
@@ -1,9 +1,21 @@
-void setup() {
-  // put your setup code here, to run once:
+int sensorPin = 0;
+int ledPin = 11;
 
+void setup() {
+  analogReference(DEFAULT);
+  Serial.begin(9600);
+  pinMode(ledPin,OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  Serial.println(analogRead(sensorPin));
+  delay(500);
+  if(analogRead(sensorPin) < 500)
+  {
+    digitalWrite(ledPin,HIGH);
+  }
+  else
+  {
+    digitalWrite(ledPin,LOW);
+  }
 }
