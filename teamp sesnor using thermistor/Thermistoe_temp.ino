7899cc3f3c13d846b3add839443260e41e0308d8
MADHAV BAHL <madhavbahl10@gmail.com>
2017-06-27 08:56:58 +0530

    CODE - 13 -- TEMPERATURE MEASUREMENT USING THERMISTOR

---
+++ teamp sesnor using thermistor/Thermistoe_temp.ino
@@ -0,0 +1,28 @@
+#include<math.h>
+
+double Thermistor(int RawADC) {
+  double Temp;
+  Temp = log(10000.0 * ((1024.0/RawADC-1)));
+  Temp = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * Temp * Temp ))*Temp );
+  Temp = Temp - 273.15;
+  Temp = (Temp * 9.0)/5.0 + 32.0;
+  return Temp;
+}
+  
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop(){
+  int val;
+  double temp;
+  val=analogRead(0);
+  temp = Thermistor(val);
+  temp = temp-32;
+  temp = temp*.5556;
+  temp=temp+1;
+  Serial.print(temp);
+  Serial.print("\n");
+  delay(1000);
+}
+
