7c40b03b99315d7d82f9bd951bf9dda8403b86c6
MADHAV BAHL <madhavbahl10@gmail.com>
2017-08-06 19:04:33 +0530

    UltraSonic Sensor : Distance Measure

---
+++ UltraSonic/UltraSonicModule/UltraSonicModule.ino
@@ -0,0 +1,68 @@
+const int trigPin = 2;
+const int echoPin = 4;
+const int ledPin = 13;
+
+void setup() {
+  // initialize serial communication:
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(ledPin,OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop()
+{
+  // establish variables for duration of the ping, 
+  // and the distance result in inches and centimeters:
+  long duration, inches, cm;
+
+  // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
+  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // Read the signal from the sensor: a HIGH pulse whose
+  // duration is the time (in microseconds) from the sending
+  // of the ping to the reception of its echo off of an object.
+  
+  duration = pulseIn(echoPin, HIGH);
+
+  // convert the time into a distance
+  inches = microsecondsToInches(duration);
+  cm = microsecondsToCentimeters(duration);
+  
+  Serial.print(inches);
+  Serial.print("in, ");
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+
+  if(inches < 30)
+    digitalWrite(ledPin,HIGH);
+  else
+    digitalWrite(ledPin,LOW); 
+  
+  delay(25);
+}
+
+long microsecondsToInches(long microseconds)
+{
+  // According to Parallax's datasheet for the PING))), there are
+  // 73.746 microseconds per inch (i.e. sound travels at 1130 feet per
+  // second).  This gives the distance travelled by the ping, outbound
+  // and return, so we divide by 2 to get the distance of the obstacle.
+  // See: http://www.parallax.com/dl/docs/prod/acc/28015-PING-v1.3.pdf
+  return microseconds / 74 / 2;
+}
+
+long microsecondsToCentimeters(long microseconds)
+{
+  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
+  // The ping travels out and back, so to find the distance of the
+  // object we take half of the distance travelled.
+  return microseconds / 29 / 2;
+}
