e88c4ff0f5f1d36b31a8dca76dfbe1f9b0f45cb1
MADHAV BAHL <madhavbahl10@gmail.com>
2017-08-26 06:41:04 +0530

    Humidity Sensor ADDED

---
+++ Humidity Sensor/dht/dht.ino
@@ -0,0 +1,56 @@
+#include <dht.h>
+
+dht DHT;
+
+#define DHT11_PIN 5
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("DHT TEST PROGRAM ");
+  Serial.print("LIBRARY VERSION: ");
+  Serial.println(DHT_LIB_VERSION);
+  Serial.println();
+  Serial.println("Type,\tstatus,\tHumidity (%),\tTemperature (C)");
+}
+
+void loop()
+{
+  // READ DATA
+  Serial.print("DHT11, \t");
+  int chk = DHT.read11(DHT11_PIN);
+ /* switch (chk)
+  {
+    case DHTLIB_OK:  
+                Serial.print("OK,\t"); 
+                break;
+    case DHTLIB_ERROR_CHECKSUM: 
+                Serial.print("Checksum error,\t"); 
+                break;
+    case DHTLIB_ERROR_TIMEOUT: 
+                Serial.print("Time out error,\t"); 
+                break;
+    case DHTLIB_ERROR_CONNECT:
+        Serial.print("Connect error,\t");
+        break;
+    case DHTLIB_ERROR_ACK_L:
+        Serial.print("Ack Low error,\t");
+        break;
+    case DHTLIB_ERROR_ACK_H:
+        Serial.print("Ack High error,\t");
+        break;
+    default: 
+                Serial.print("Unknown error,\t"); 
+                break;
+  }*/
+  // DISPLAY DATA
+  Serial.print(DHT.humidity, 1);
+  Serial.print(",\t");
+  Serial.println(DHT.temperature, 1);
+
+  delay(2000);
+}
+//
+// END OF FILE
+//
+
---
+++ Humidity Sensor/try2/try2/try2.ino
@@ -0,0 +1,31 @@
+#include <dht.h>
+#define dht_apin A0 // Analog Pin sensor is connected to
+ 
+dht DHT;
+ 
+void setup(){
+ 
+  Serial.begin(9600);
+  delay(500);//Delay to let system boot
+  Serial.println("DHT11 Humidity & temperature Sensor\n\n");
+  delay(1000);//Wait before accessing Sensor
+ 
+}//end "setup()"
+ 
+void loop(){
+  //Start of Program 
+ 
+    DHT.read11(dht_apin);
+    
+    Serial.print("Current humidity = ");
+    Serial.print(DHT.humidity);
+    Serial.print("%  ");
+    Serial.print("temperature = ");
+    Serial.print(DHT.temperature); 
+    Serial.println("C  ");
+    
+    delay(100);//Wait 5 seconds before accessing sensor again.
+ 
+  //Fastest should be once every two seconds.
+ 
+}
